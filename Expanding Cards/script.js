25e31b40ed9f43890e772949c94daab04debd3ef
Gargie <gargiesingh321@gmail.com>
2025-07-16 00:25:11 +0530

    Added Js file

--- Expanding Cards/script.js
+++ Expanding Cards/script.js
@@ -1,1 +1,11 @@
- 
+const panels = document.querySelectorAll(".panel");
+console.log(panels)
+panels.forEach(panel=>{
+    panel.addEventListener("click", (event)=>{
+        if(panel.classList.contains("active")){
+        panel.classList.remove("active");
+    }
+    event.target.classList.add("active");
+    })
+    
+})
