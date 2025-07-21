a51e6eb83e3ab8a7bbc923fe58da6f8f3369b81e
Gargie <gargiesingh321@gmail.com>
2025-07-21 20:51:27 +0530

    Fixed the js code

--- Scroll Animation/script.js
+++ Scroll Animation/script.js
@@ -1,9 +1,19 @@
 const boxes = document.querySelectorAll(".box");
+boxes.forEach(box =>{
+        const boxTop = box.getBoundingClientRect().top;
 
+        if(boxTop < window.innerHeight/1.2){
+            box.classList.add("show")
+        }
+        else{
+            box.classList.remove("show")
+        }
+    })
 window.addEventListener("scroll", (event)=>{
     boxes.forEach(box =>{
         const boxTop = box.getBoundingClientRect().top;
-        if(boxTop < window.innerHeight/5){
+
+        if(boxTop < window.innerHeight/1.2){
             box.classList.add("show")
         }
         else{
