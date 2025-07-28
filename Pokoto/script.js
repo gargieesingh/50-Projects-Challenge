a8af791dd5a7263d1ffdf61a363890fa8e13f633
Gargie <gargiesingh321@gmail.com>
2025-07-28 23:45:54 +0530

    Added Mouse Events

--- Pokoto/script.js
+++ Pokoto/script.js
@@ -2,11 +2,30 @@ const canvas = document.getElementById('canvas');
 const contxt = canvas.getContext('2d');
 let x;
 let y;
-
+let isPressed = false;
+//MOUSE EVENTS
+canvas.addEventListener('mousedown', (event)=>{
+    isPressed = true;
+})
+canvas.addEventListener('mouseup', (event)=>{
+    isPressed = false;
+    x = undefined , y = undefined;
+})
+canvas.addEventListener('mousemove', (event)=>{
+    if(isPressed){
+        x0 = event.offsetX;
+        y0 = event.offsetY;
+        drawCircle(x0,y0);
+        drawLine(x, y, x0, y0);
+        x = x0;
+        y = y0;
+    }
+})
+//CANVAS SHAPES
 function drawCircle(x,y){
     contxt.beginPath();
-    contxt.arc(x, y, 10, 0, Math.PI *2);
-    contxt.fillStyle;
+    contxt.arc(x, y, size, 0, Math.PI *2);
+    contxt.fillStyle = color;
     contxt.fill();
 }
 
@@ -14,8 +33,8 @@ function drawLine(x,y, x2, y2){
     contxt.beginPath();
     contxt.moveTo(x,y);
     contxt.lineTo(x2, y2);
-    contxt.strokeStyle = "black";
-    contxt.lineWidth = 20;
+    contxt.strokeStyle = color;
+    contxt.lineWidth = size * 2;
     contxt.stroke();
 }
 
