c5412dc9a5fa5a22bd7bd4dd3111a2e9c92f325b
Gargie <gargiesingh321@gmail.com>
2025-07-28 23:15:34 +0530

    Done with basic shapes in canvas

--- Pokoto/script.js
+++ Pokoto/script.js
@@ -1,11 +1,21 @@
 const canvas = document.getElementById('canvas');
 const contxt = canvas.getContext('2d');
-
+let x;
+let y;
 
 function drawCircle(x,y){
     contxt.beginPath();
-    contxt.arc(x, y, 20, 0, Math.PI *2);
-    contxt.fillStyle = color;
+    contxt.arc(x, y, 10, 0, Math.PI *2);
+    contxt.fillStyle;
     contxt.fill();
 }
-drawCircle(100, 100);
+
+function drawLine(x,y, x2, y2){
+    contxt.beginPath();
+    contxt.moveTo(x,y);
+    contxt.lineTo(x2, y2);
+    contxt.strokeStyle = "black";
+    contxt.lineWidth = 20;
+    contxt.stroke();
+}
+
