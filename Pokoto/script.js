4e198cbdb133adc4a3d75fd36a0c654304bdf217
Gargie <gargiesingh321@gmail.com>
2025-07-29 16:31:10 +0530

    Added functionality in buttons

--- Pokoto/script.js
+++ Pokoto/script.js
@@ -1,11 +1,52 @@
 const canvas = document.getElementById('canvas');
 const contxt = canvas.getContext('2d');
+const decrease = document.getElementById("decrease");
+const increase = document.getElementById("increase");
+const sizeBox = document.getElementById("size");
+const colorBox = document.getElementById("color");
+const clear = document.getElementById("clear");
+
 let x;
 let y;
 let isPressed = false;
+
+colorBox.addEventListener('change', (event)=>{
+    color = event.target.value;
+})
+
+clear.addEventListener('click' , ()=> contxt.clearRect(0, 0, canvas.width , canvas.height))
+
+var size = parseInt(sizeBox.textContent);
+decrease.addEventListener("click", (event)=>{
+    let currentSize = parseInt(sizeBox.textContent);
+    if(currentSize > 2){
+        currentSize -= 2;
+        sizeBox.textContent = currentSize;
+        size = currentSize;
+    }
+    else{
+        alert("This is the minimum brush size!");
+    }
+})
+
+increase.addEventListener('click', ()=>{
+    let currentSize = parseInt(sizeBox.textContent);
+    if(currentSize < 80){
+        currentSize += 2;
+        sizeBox.textContent = currentSize;
+        size = currentSize;
+    }
+    else{
+        alert("This is the maximum brush size!")
+    }
+})
+
+
 //MOUSE EVENTS
 canvas.addEventListener('mousedown', (event)=>{
     isPressed = true;
+    x = event.offsetX;
+    y = event.offsetY;
 })
 canvas.addEventListener('mouseup', (event)=>{
     isPressed = false;
@@ -37,4 +78,5 @@ function drawLine(x,y, x2, y2){
     contxt.lineWidth = size * 2;
     contxt.stroke();
 }
+
 
