6ff049ee09c06f6be04bb7678bf6cc6ff8dc8d3f
Gargie <gargiesingh321@gmail.com>
2025-08-04 23:06:43 +0530

    Added js file

---
+++ Temp Converter/script.js
@@ -0,0 +1,5 @@
+const celsius = document.querySelector('#cel');
+const fahrenheit = document.querySelector('#fahr');
+const kelvin = document.querySelector('#kel');
+const h2 = document.querySelector('h2');
+console.log(h2);
