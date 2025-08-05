7c870fa227f95b33b01f195bb8f9e23ddc2c1be1
Gargie <gargiesingh321@gmail.com>
2025-08-05 23:37:07 +0530

    Updated basic js logic for temperature conversion

--- Temp Converter/script.js
+++ Temp Converter/script.js
@@ -1,5 +1,19 @@
-const celsius = document.querySelector('#cel');
-const fahrenheit = document.querySelector('#fahr');
-const kelvin = document.querySelector('#kel');
-const h2 = document.querySelector('h2');
-console.log(h2);
+const temps = document.querySelectorAll('input');
+temps.forEach(temp =>{
+    temp.addEventListener('change', (event)=>{
+        if(temp == temps[0]){
+            celValue = +(temp.value);
+            temps[1].value = ((celValue * 9)/5 + 32).toFixed(2);
+            temps[2].value = (celValue + 273.15).toFixed(2);
+        }else if(temp == temps[1]){
+            fahrValue = +(temp.value);
+            temps[0].value = ((fahrValue -32) * 5/9).toFixed(2);
+            temps[2].value = ((((fahrValue - 32) * 5)/9) + 273.15).toFixed(2);
+        }
+        else{
+            kelValue = +(temp.value);
+            temps[0].value = (kelValue - 273.15).toFixed(2);
+            temps[1].value = ((temps[0].value * 9)/5 + 32).toFixed(2);
+        }
+    })
+})
