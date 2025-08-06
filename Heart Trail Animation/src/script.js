e922b1744559d2e5eaf5865cb24e8a40e0bc5d4a
Gargie <gargiesingh321@gmail.com>
2025-08-06 16:30:24 +0530

    added setTimeout

--- Heart Trail Animation/src/script.js
+++ Heart Trail Animation/src/script.js
@@ -4,11 +4,23 @@ const box = document.querySelector('.box');
 box.addEventListener('mousemove', (event)=>{
     let X = event.offsetX;
     let Y = event.offsetY;
-    heart.style.left= `${X}px`;
-    heart.style.top= `${Y}px`;
+
+    //new span
     let newHeart = document.createElement('span');
     newHeart.className = heart.className;
-    box.appendChild(newHeart);
+
+    //span positioning
     newHeart.style.left= `${X}px`;
     newHeart.style.top= `${Y}px`;
+
+    //hearts of different size
+    const size = (Math.random() * 50).toFixed(3);
+    newHeart.style.width =size + "px";
+    newHeart.style.height = size + "px";
+
+    box.appendChild(newHeart);
+
+    setTimeout(()=>{
+        newHeart.remove();
+    }, 10000)
 })
