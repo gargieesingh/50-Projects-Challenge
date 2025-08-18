00ea7df7b599ab44778eaa64aeb2f3f2f3b8dc72
Gargie <gargiesingh321@gmail.com>
2025-08-18 23:57:49 +0530

    Added basic variables in js

--- git profile finder/script.js
+++ git profile finder/script.js
@@ -1,6 +1,12 @@
 const searchBtn = document.querySelector('button');
 const input = document.querySelector('input');
 console.dir(input);
+
 searchBtn.addEventListener('click', (event)=>{
     input.value = "";
+    let username = document.getElementById('username').value;
+    let profileDiv = document.getElementById('profile');
+    if(username === ""){
+        profileDiv.innerHTML = "<p>Please enter a username !</p>"
+    }
 })
